002706104177246abef019cf574ed73c1879bada
Danilo Quispe <dql@daniloquispe.dev>
2024-07-12 17:49:02 -0500

    Remove "create" button from information pages resource
    Information pages list is fixed, no page creation is allowed in this CMS version, so a "create" button is a nonsense here.

--- app/Filament/Resources/PageResource/Pages/ListPages.php
+++ app/Filament/Resources/PageResource/Pages/ListPages.php
@@ -9,11 +9,4 @@ use Filament\Resources\Pages\ListRecords;
 class ListPages extends ListRecords
 {
     protected static string $resource = PageResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
