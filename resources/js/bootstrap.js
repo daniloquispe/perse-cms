4a614d019b1d744caf91e07d9d18941a71b44cce
Danilo Quispe <dql@daniloquispe.dev>
2024-08-02 03:47:45 -0500

    Install Roboto font

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
