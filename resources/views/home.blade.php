79bc5933898f07264dbe19e6951615bf28931ee2
Danilo Quispe <dql@daniloquispe.dev>
2024-08-07 14:03:45 -0500

    Update CSS stylesheets for mobile devices

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,11 @@
 @section('title', 'Persé Librerías')
 
 @section('content')
+	{{-- Main slider --}}
 	<livewire:slider />
-	{{-- Banners --}}
+	{{-- Banners pequeños --}}
 	<div class="container-box my-12">
-		<div class="grid grid-cols-3 gap-10">
+		<div class="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-10">
 			<div><img src="{{ asset('images/placeholders/banner.png') }}" alt="Banner" class="w-full rounded-lg" /></div>
 			<div><img src="{{ asset('images/placeholders/banner.png') }}" alt="Banner" class="w-full rounded-lg" /></div>
 			<div><img src="{{ asset('images/placeholders/banner.png') }}" alt="Banner" class="w-full rounded-lg" /></div>
