b1bf1b5219e10f047683db0419a948452cfcb64f
Danilo Quispe <dql@daniloquispe.dev>
2024-08-11 04:49:45 -0500

    Remove HasFactory trait from BookbindingType model definition
    This model has no factory, so the `use HasFactory` instruction is not needed.

--- app/Models/BookbindingType.php
+++ app/Models/BookbindingType.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BookbindingType extends Model
 {
-    use HasFactory;
-
-	protected $fillable = ['name'];
+    protected $fillable = ['name'];
 
 	public function books(): HasMany
 	{
