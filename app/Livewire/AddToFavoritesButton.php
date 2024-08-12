aef3f22b5d0607e78588ada58097b58a05f0cc6d
Danilo Quispe <dql@daniloquispe.dev>
2024-08-12 13:13:42 -0500

    Use toast in "add to favorites" button when user is not logged in
    Pending:
    - Route cache and route names

--- app/Livewire/AddToFavoritesButton.php
+++ app/Livewire/AddToFavoritesButton.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Page;
+use App\Models\SeoTags;
+use App\PageRole;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class AddToFavoritesButton extends Component
 {
+	use \App\Toast;
+
 	public int $bookId;
 
 	public bool $isFavorite = false;
@@ -32,7 +37,17 @@ class AddToFavoritesButton extends Component
 	public function save(): void
 	{
 		if (!Auth::guard('storefront')->check())
+		{
+			// Login route
+			$loginPageSlug = SeoTags::query()
+				->where('owner_id', PageRole::Login->value)
+				->where('owner_type', Page::class)
+				->value('slug');
+
+			$this->toast('Necesitas iniciar sesión antes de agregar un producto a la lista', link: "/$loginPageSlug", linkText: 'Iniciar sesión');
+
 			return;
+		}
 
 		$favoritesQuery = Auth::guard('storefront')->user()->favorites();
 
