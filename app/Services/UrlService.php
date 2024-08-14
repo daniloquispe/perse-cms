5954ce20fecf4d69b361488c90ea98bf230e0f08
Danilo Quispe <dql@daniloquispe.dev>
2024-08-14 02:08:23 -0500

    New service class for site URLs
    This class can obtain full URLs from a page role (for information pages only) or directly from a page slug (for an information page, book or book category).

---
+++ app/Services/UrlService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Page;
+use App\Models\SeoTags;
+use App\PageRole;
+use Illuminate\Database\Eloquent\Builder;
+
+class UrlService
+{
+	public function fromPageRole(PageRole|int $role): string|null
+	{
+		if ($role instanceof PageRole)
+			$role = $role->value;
+
+		$slug = SeoTags::query()
+			->where('owner_type', Page::class)
+			->whereHas('owner', function (Builder $query) use ($role): Builder
+			{
+				return $query->where('is_visible', true)->whereKey($role);
+			})
+			->value('slug');
+
+		return $this->fromSlug($slug);
+	}
+
+	public function fromSlug(?string $slug): string|null
+	{
+		return $slug ? url($slug) : null;
+	}
+}
