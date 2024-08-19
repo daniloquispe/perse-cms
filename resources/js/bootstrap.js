a9a5c0c08e023596e95f10ed7e7939a9fceeb92d
Danilo Quispe <dql@daniloquispe.dev>
2024-08-19 08:42:01 -0500

    Install BugSnag Performance
    Reference: https://docs.bugsnag.com/performance/integration-guides/web/

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,10 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/* BugSnag */
+import BugsnagPerformance from '@bugsnag/browser-performance';
+BugsnagPerformance.start({
+	apiKey: '5de183f969f3ff7d0197a1b1031d45c4',
+	releaseStage: import.meta.env.PROD ? 'production' : 'local'
+});
