43ea4cc72992e3b28ca3be52f3f5e1fcba68878b
Danilo Quispe <dql@daniloquispe.dev>
2024-08-26 22:52:58 -0500

    Add responsive breakpoints to book carousels
    Breakpoints are based on Tailwind CSS breakpoints, including the custom "3xl" breakpoint prefix.
    
    References:
    - https://splidejs.com/guides/options/#breakpoints
    - https://tailwindcss.com/docs/responsive-design

--- resources/js/livewire/book-carousel.js
+++ resources/js/livewire/book-carousel.js
@@ -4,5 +4,31 @@ let carousels = document.querySelectorAll('.book-carousel .splide');
 
 carousels.forEach(function (carousel)
 {
-	new Splide(carousel, {gap: '1rem', perPage: 6, perMove: 1}).mount();
+	const options = {
+		gap: '1rem',
+		perMove: 1,
+		perPage: 6,
+		breakpoints: {
+			640: {
+				perPage: 2,
+			},
+			768: {
+				perPage: 2,
+			},
+			1024: {
+				perPage: 2,
+			},
+			1280: {
+				perPage: 3,
+			},
+			1536: {
+				perPage: 4,
+			},
+			1678: {
+				perPage: 5,
+			},
+		}
+	};
+
+	new Splide(carousel, options).mount();
 });
