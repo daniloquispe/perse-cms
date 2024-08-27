b31f028a364cfb881dad2ff9ee2495e4d40061ee
Danilo Quispe <dql@daniloquispe.dev>
2024-08-27 15:27:27 -0500

    Update home page layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,22 +7,22 @@
 	<livewire:slider />
 	{{-- Book carousels (above) --}}
 	@foreach($carouselsAbove as $carousel)
-		<section class="container-box my-12">
+		<section class="container-box mb-9">
 			<livewire:book-carousel :wire:key="$carousel->id" :carousel="$carousel" />
 		</section>
 	@endforeach
 	{{-- Big banner --}}
-	<section class="container-box my-12">
+	<section class="container-box mb-9 pt-1">
 		<img src="{{ asset('images/placeholders/wide-banner.png') }}" alt="Banner" class="w-full rounded-lg" />
 	</section>
 	{{-- Book carousels (below) --}}
 	@foreach($carouselsBelow as $carousel)
-		<section class="container-box my-12">
+		<section class="container-box mb-9">
 			<livewire:book-carousel :wire:key="$carousel->id" :carousel="$carousel" />
 		</section>
 	@endforeach
 	{{-- Small banners --}}
-	<section class="container-box my-12">
+	<section class="container-box mb-11 pt-2">
 		<div class="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-10">
 			<div><img src="{{ asset('images/placeholders/banner.png') }}" alt="Banner" class="w-full rounded-lg" /></div>
 			<div><img src="{{ asset('images/placeholders/banner.png') }}" alt="Banner" class="w-full rounded-lg" /></div>
