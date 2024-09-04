75b937867b03a405eeb5d17ba3e81751d14f928f
Danilo Quispe <dql@daniloquispe.dev>
2024-09-04 05:11:55 -0500

    Cart: Steps indicator

--- resources/views/livewire/cart/panel.blade.php
+++ resources/views/livewire/cart/panel.blade.php
@@ -1,31 +1,43 @@
 <div>
-	{{-- Step indicator --}}
-	<ul>
-		<li>
-			<a href="{{ route('cart.list') }}">
-				@if($step == 1)
-					Paso 1
-				@endif
-				Carro
-			</a>
-		</li>
-		<li>
-			<a href="{{ route('cart.list') }}">
-				@if($step == 2)
-					Paso 2
-				@endif
-				Entrega
-			</a>
-		</li>
-		<li>
-			<a href="{{ route('cart.list') }}">
-				@if($step == 3)
-					Paso 3
-				@endif
-				Pago
-			</a>
-		</li>
-	</ul>
+	{{-- Steps indicator --}}
+	<div class="cart-steps-wrapper">
+		<div class="line"></div>
+		<ul class="cart-steps">
+			<li>
+				<a href="{{ route('cart.list') }}">
+					<div @class(['marker', 'current' => $step == 1])>
+						<x-icons.cart />
+						@if($step == 1)
+							<div class="text-sm text-nowrap">Paso 1</div>
+						@endif
+					</div>
+					<div>Carro</div>
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('cart.delivery') }}">
+					<div @class(['marker', 'current' => $step == 2])>
+						<x-icons.truck />
+						@if($step == 2)
+							Paso 2
+						@endif
+					</div>
+					<div>Entrega</div>
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('cart.list') }}">
+					<div @class(['marker', 'current' => $step == 3])>
+						<x-icons.credit-card />
+						@if($step == 3)
+							Paso 3
+						@endif
+					</div>
+					<div>Pago</div>
+				</a>
+			</li>
+		</ul>
+	</div>
 	{{-- Main content --}}
 	@if($step == 1)
 		<livewire:cart.products-list-section />
