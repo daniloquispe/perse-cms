92c4f85b2bed9e22344479e1afb81f710e5540d1
Danilo Quispe <dql@daniloquispe.dev>
2024-09-11 11:59:15 -0500

    Bugfix: "Reset" button in search form partially covers search box placeholder, even if button visibility is set to "hidden"

--- resources/views/livewire/search-form.blade.php
+++ resources/views/livewire/search-form.blade.php
@@ -3,12 +3,14 @@
 		{{-- Search --}}
 		<input type="search" wire:model.live="searchString" wire:focus="markShowResetButton" x-on:blur="$wire.markDontShowResetButton()" placeholder="Busca por título, autor, género o ISBN" aria-label="Buscar" />
 		{{-- Reset button --}}
-		<button type="reset" title="Borrar" style="{{ !empty($searchString) && $canShowResetButton ? 'visibility: visible' : 'visibility: hidden' }}">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-			</svg>
-			<span>Borrar</span>
-		</button>
+		@if(!empty($searchString) && $canShowResetButton)
+			<button type="reset" title="Borrar">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+				</svg>
+				<span>Borrar</span>
+			</button>
+		@endif
 		{{-- Search button --}}
 		<button type="submit" title="Buscar">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
