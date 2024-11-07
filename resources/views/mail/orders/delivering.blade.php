87e130e55c81545d04d0293c0fd23749e2956ff7
Danilo Quispe <dql@daniloquispe.dev>
2024-11-06 20:56:22 -0500

    Changes in order mail notifications
    Changes:
    - Update phone number
    - Update CSS styles

--- resources/views/mail/orders/delivering.blade.php
+++ resources/views/mail/orders/delivering.blade.php
@@ -30,7 +30,8 @@
 					p
 					{
 						font-size: 0.9rem;
-						margin-bottom: 1.1rem;
+						margin-bottom: 1rem;
+						line-height: 1.3rem;
 
 						&:last-child
 						{
@@ -55,6 +56,11 @@
 				{
 					display: flex;
 					background-color: #ff8300;
+
+					svg
+					{
+						stroke-width: 1.5;
+					}
 				}
 
 				/*p
@@ -306,7 +312,7 @@
 
 		.table-wrapper
 		{
-			margin: 0.5rem 1rem 1rem;
+			margin: 1.5rem 1rem 1rem;
 		}
 
 		.products-table
@@ -620,7 +626,7 @@
 						</div>
 						<div>
 							<x-icons.headset />
-							<p>Llámanos al<br /><span>(01) 981 306-233<br />de lunes a domingo de 8am a 8pm</span></p>
+							<p>Llámanos al<br /><span>(01) 960&nbsp;155&nbsp;037<br />de lunes a domingo de 8am a 8pm</span></p>
 						</div>
 					</div>
 				</section>
