b901320f9f5906290b89bb13dfff47b042f14333
Danilo Quispe <dql@daniloquispe.dev>
2024-11-11 16:57:41 -0500

    Implement footer subscription form

--- app/Livewire/SiteFooter.php
+++ app/Livewire/SiteFooter.php
@@ -2,12 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\FooterSubscriptionForm;
+use App\PageRole;
+use App\Services\UrlService;
+use App\Toast;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class SiteFooter extends Component
 {
-    public function render()
+	use Toast;
+
+	public FooterSubscriptionForm $subscriptionForm;
+
+    public function render(UrlService $urlService): View
     {
-        return view('livewire.site-footer');
+		$termsUrl = $urlService->fromPageRole(PageRole::Terms);
+		$privacyPolicy = $urlService->fromPageRole(PageRole::PrivacyPolicy);
+
+		$data = compact('termsUrl', 'privacyPolicy');
+        return view('livewire.site-footer', $data);
     }
+
+	public function subscribe(): void
+	{
+		if ($this->subscriptionForm->submit())
+		{
+			$this->toast('¡Gracias por suscribirte!');
+			$this->subscriptionForm->reset();
+		}
+		else
+			$this->toast('No hemos podido suscribirte', 'Por favor, vuelve a intentarlo');
+	}
 }
